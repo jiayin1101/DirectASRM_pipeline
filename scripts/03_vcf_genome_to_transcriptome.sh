337e3bfecb55ace6d1112d8e21d8547071771896
jiayin1101 <1434496855@qq.com>
2026-04-28 23:05:21 +0800

    Update 03_vcf_genome_to_transcriptome.sh

--- scripts/03_vcf_genome_to_transcriptome.sh
+++ scripts/03_vcf_genome_to_transcriptome.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 source "$(dirname "$0")/../config/config.sh"
-IN_VCF="${RESULTS_DIR}/02_genome_alignment_and_variant_calling/longshot.genome.vcf"
+
+IN_VCF="${RESULTS_DIR}/02_genome_alignment_and_variant_calling/longshot.genome.biallelic_snps.vcf"
 OUT_DIR="${RESULTS_DIR}/03_transcriptome_vcf"
+
 mkdir -p "${OUT_DIR}"
 
 Rscript utils/vcf_g2t_human.R \
